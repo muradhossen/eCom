4ce65aa3b96389a9efe73c266cb5dfeba4ba2a01
muradhossenEN <murad@enlightsolution.com>
2024-02-05 00:20:43 +0600

    added search view migration

---
+++ Infrastructure/Migrations/20240204181910_added-search-view.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedsearchview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Sections_ProductId",
+                table: "Sections");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sections_ProductId",
+                table: "Sections",
+                column: "ProductId",
+                unique: true);
+
+            migrationBuilder.Sql(@"
+CREATE OR REPLACE VIEW ""VwSearch""
+AS
+SELECT
+p.""Id"" AS ""ItemId"",
+p.""Code"",
+p.""Name"", 
+p.""ImageUrl"",
+pi.""Price"",
+CASE 
+	WHEN pi.""Id"" ISNULL THEN FALSE
+	ELSE TRUE END AS ""HasPricing"",
+'Product' AS ""Type"",
+p.""SubCategoryId"" 
+FROM ""Products"" p
+LEFT JOIN ""Sections"" s on s.""ProductId"" = p.""Id""
+LEFT JOIN ""PricingItems"" pi on pi.""SectionId"" = s.""Id""
+WHERE p.""IsDeleted"" = FALSE");
+
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Sections_ProductId",
+                table: "Sections");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sections_ProductId",
+                table: "Sections",
+                column: "ProductId");
+
+            migrationBuilder.Sql(@"DROP VIEW ""VwSearch""");
+        }
+    }
+}
